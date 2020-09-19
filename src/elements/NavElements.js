395a40c050497a257e67c891ba472441b39c01ec
nikub444 <magaszak12@gmail.com>
2020-09-19 16:41:35 +0200

    added footer

--- src/elements/NavElements.js
+++ src/elements/NavElements.js
@@ -6,7 +6,11 @@ export const NavWrapper  = styled.nav`
     display: flex;
     align-items: center;
 
+    & img {
+        height: 30px;
+    }
+
     @media ${props => props.theme.breakpoints.tablet}{
-        grid-column: 2/ span 6;
+        grid-column: 2 / span 6;
     }
 `
