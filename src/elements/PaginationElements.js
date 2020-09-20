339ce9cfe33b85cd0a7e8019c8156a62dde70f6d
nikub444 <magaszak12@gmail.com>
2020-09-20 19:07:36 +0200

    created posts

--- src/elements/PaginationElements.js
+++ src/elements/PaginationElements.js
@@ -11,13 +11,13 @@ export const PaginationWrapper = styled.div`
 
   a:nth-child(1) {
     color: ${props =>
-      props.isFirst ? props.theme.color.dark3 : props.theme.color.dark1};
+      props.isFirst ? props.theme.colors.dark3 : props.theme.colors.dark1};
     pointer-events: ${props => (props.isFirst ? "none" : "auto")};
     cursor: ${props => (props.isFirst ? "default" : "pointer")};
   }
   a:nth-child(2) {
     color: ${props =>
-      props.isLast ? props.theme.color.dark3 : props.theme.color.dark1};
+      props.isLast ? props.theme.colors.dark3 : props.theme.colors.dark1};
     pointer-events: ${props => (props.isLast ? "none" : "auto")};
     cursor: ${props => (props.isLast ? "default" : "pointer")};
   }
